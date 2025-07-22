b05c2163e2762429aef1eea07a06e3855ff2e7d3
Tuandu <tuanduvip1@gmail.com>
2025-07-22 20:46:33 +0700

    mock data interface chat

--- src/components/Header/HeaderUser.jsx
+++ src/components/Header/HeaderUser.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Input, Button, Badge, Menu, Drawer, Dropdown, Avatar } from "antd";
+import { Input, Button, Menu, Drawer, Dropdown, Avatar } from "antd";
 import {
     SearchOutlined,
     MenuOutlined,
@@ -18,13 +18,11 @@ import Logo from "./Logo";
 import { useGetAllCategoryUserQuery } from "@/redux/category/category.query";
 import { useGetAllBrandByUserQuery } from "@/redux/brand/brand.query";
 import Loading from "../Loading/Loading";
-import { IoCartOutline, IoNotificationsOutline } from "react-icons/io5";
+import { IoCartOutline } from "react-icons/io5";
 import NotificationDrop from "./NotificationStoreDrop";
 import LanguageSelector from "../language/LanguageSelector";
 import { useTranslation } from "react-i18next";
-import Promotion from "@/pages/Promotion";
 import { useGetAllActivePromotionsQuery } from "@/redux/promotion/promotion.query";
-import { message } from "antd";
 
 const HeaderUser = () => {
     const dispatch = useDispatch();
@@ -167,7 +165,7 @@ const HeaderUser = () => {
                     {
                         label: "Lịch sử đơn hàng",
                         key: "orderHistory",
-                        path: "#",
+                        path: "/account?tab=orders",
                     },
                     {
                         label: "Đăng xuất",
